3266c8600a4b61c14698eeb59ccc12d1fb0a1448
astravaganza <83530103+astravaganza@users.noreply.github.com>
2024-10-04 05:32:21 +0530

    Updated fingerprintable values

--- config.py
+++ config.py
@@ -11,12 +11,17 @@ MASTER_AES_KEY = b'w3Ypsr5rtifP1ioA'
 AES_START = b'1ioA'
 AES_END = b'w3Yp'
 
+CERT_FP = '84:5F:C4:C3:4D:C7:5E:F5:C8:09:4C:4B:B5:D9:FE:23:83:41:4E:74'
+
 DEFAULT_HEADERS = {
     'serviceName': 'ManoramaMax',
-    'X-myplex-AppVersion': '7014',
+    'X-myplex-AppVersion': '7087',
     'X-myplex-platform': 'Android',
-    'User-Agent': 'okhttp/4.9.1'
+    'User-Agent': 'okhttp/5.0.0-alpha.7'
 }
+
+UA_MMAX = 'manoramaMAX/2.0.86 (Android 13;49c196d03e009b8cBuild/ Redmi Note 12 4G)'
+PACKAGE_NAME = 'com.mmtv.manoramamax.android'
 
 MANIFEST_PAYLOAD = b'{"fields":"videos,videoInfo,subtitles,generalInfo,' \
                    b'content,thumbnailSeekPreview,skipConfig","mcc":"404"' \
